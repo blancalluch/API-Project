081ee3ede60b03482dcc9f207e14102bceb9e248
Blanca Lluch <lluchponceblanca@gmail.com>
2019-12-02 20:34:10 +0100

    sentiments

--- mongoAtlasConnection.py
+++ mongoAtlasConnection.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 from pymongo import MongoClient
 import getpass
 import json
@@ -7,21 +6,30 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-#Get Password
+# Get Password
 connection = os.getenv("MONGODB_CONNECT")
 
-#Connect to DB
+# Connect to DB
 client = MongoClient(connection)
+
+
 def connectCollection(database, collection):
     '''Connecting with mongoDB and reading database collection.'''
     db = client[database]
     coll = db[collection]
     return db, coll
 
-#url='./input/chats.json'
-def add_json(rem,coll,url):
+
+def add_json(rem, coll, url):
     with open(url) as f:
         chats_json = json.load(f)
-    if rem:    
+    if rem:
         coll.remove({})
     coll.insert_many(chats_json)
+
+
+db, coll = connectCollection('chat_conversations', 'chat_conversations')
+db_sent, coll_sent = connectCollection('chat_conversations', 'sentiments')
+coll_sent.remove({})
+#url = './input/chats.json'
+#add_json(1, coll, url)

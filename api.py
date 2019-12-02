516a4f81eb7b935f33f865cdf13f3dec897d56aa
Blanca Lluch <lluchponceblanca@gmail.com>
2019-12-02 16:31:10 +0100

    add message

--- api.py
+++ api.py
@@ -3,11 +3,12 @@ import random
 import mongoAtlasConnection as mon
 import bson
 from bson.json_util import dumps
+import datetime
 
 
 db, coll = mon.connectCollection('chat_conversations','chat_conversations')
-#url='./input/chats.json'
-#mon.add_json(1,coll,url)
+url='./input/chats.json'
+mon.add_json(1,coll,url)
 @get("/")
 def index():
     return dumps(coll.find())
@@ -50,36 +51,66 @@ def creatChat():
     for u in users_list:
         user={}
         user_info=(coll.find({"idUser":int(u)},{"userName":1}))
-        user["idUser"]=u
+        user["idUser"]=int(u)
         user['userName']=user_info[0]["userName"]
         user['idChat']=c
         info.append(user)
     coll.insert_many(info)
     return f"Chat_id: {c}"
-
-
-
-
 
 @post("/chat/<chat_id>/adduser")
 def addUsertoChat(chat_id):
-    c=max(coll.distinct("idChat"))+1
-    users_list=list(request.forms.getall("user_id"))
-    print(users_list)
-    info=[]
-    for u in users_list:
-        user={}
-        user_info=(coll.find({"idUser":int(u)},{"userName":1}))
-        user["idUser"]=u
-        print(user_info)
-        user['userName']=user_info[0]["userName"]
-        user['idChat']=c
-        print(user)
-        info.append(user)
-    coll.insert_many(info)
-    return f"Chat_id: {c}"
+    res="ok"
+    user=request.forms.get("user_id")
+    print(user)
+    user_info=list(coll.find({"idUser":int(user)},{"userName":1,"idChat":1,"_id":0}))
+    print(user_info)
+    for u in user_info:
+        if u["idChat"]==int(chat_id):
+            res= f"User {user} already in chat {chat_id}"
+    if res=="ok":   
+        info={
+            "idUser":int(user),
+            "userName":user_info[0]["userName"],
+            "idChat": int(chat_id)
+            }
+        coll.insert_one(info)
+        return f"Chat_id: {chat_id}"
+    else:
+        return res
 
 
+
+
+
+
+
+@post("/chat/<chat_id>/addmessage")
+def addMessagetoChat(chat_id):
+    res="ok"
+    user=request.forms.get("user_id")
+    chat=request.forms.get("chat_id")
+    text=request.forms.get("text")
+    print(user)
+    user_info=list(coll.find({"idUser":int(user)},{"userName":1,"idChat":1,"_id":0}))
+    print(user_info)
+    message_id=max(coll.distinct("idMessage"))+1
+    for u in user_info:
+        if u["idChat"]==int(chat_id):
+            res= f"User {user} already in chat {chat_id}"
+    if res=="ok":   
+        info={
+            "idUser":int(user),
+            "userName":user_info[0]["userName"],
+            "idChat": int(chat),
+            "idMessage":message_id,
+            "datetime": datetime.datetime.utcnow(),
+            "text": text
+            }
+        coll.insert_one(info)
+        return f"Chat_id: {chat_id}"
+    else:
+        return res
 
 
 

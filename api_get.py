33da8fcf9ecf9757576e91c47cc7ba943c60c406
Blanca Lluch <lluchponceblanca@gmail.com>
2019-12-02 20:34:55 +0100

    sent

---
+++ api_get.py
@@ -0,0 +1,61 @@
+from bottle import route, run, get, post, request
+import random
+from mongoAtlasConnection import coll, db, coll_sent, db_sent
+import bson
+from bson.json_util import dumps
+import api_post
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from functools import reduce
+
+
+@get("/")
+def index():
+    '''devuelve toda la informacion de la api'''
+    return dumps(coll.find())
+
+
+@get("/usernames")
+def usernames():
+    '''devuelve los usernames de la api'''
+    return dumps(coll.find({}, {"userName": 1}))
+
+
+@get("/chats")
+def chats():
+    '''devuelve los chats de la api'''
+    print(f"Chat")
+    return dumps(coll.find({}, {"idChat": 1}))
+
+
+@get("/chat/<chat_id>/list")
+def oneChat(chat_id):
+    '''devuelve una lista con los mensajes del chat indicado'''
+    data = list(coll.find({"idChat": int(chat_id)}, {"_id": 0}))
+    return {
+        "messages": [[d["idUser"], d["idMessage"], d["text"]] for d in data]
+    }
+
+
+@get("/chat/<chat_id>/sentiment")
+def chatSentiment(chat_id):
+    texts = oneChat(chat_id)
+    sid = SentimentIntensityAnalyzer()
+    sentiments = []
+    for d in list(texts['messages']):
+        info_m = {}
+        info_m["idUser"] = d[0]
+        info_m["idMessage"] = d[1]
+        info_m["Message"] = d[2]
+        info_m["Sentiment"] = sid.polarity_scores(d[2])
+        coll_sent.insert_one(info_m)
+        sentiments.append(info_m)
+    # coll_sent.insert_many(sentiments)
+    comp_sent = [s['Sentiment']['compound'] for s in sentiments]
+    avg = reduce((lambda x, y: x+y), comp_sent)/len(comp_sent)
+    return {"sentiments": sentiments,
+            "compound sentiments": comp_sent,
+            "avg sentiment [-1,1]": avg}
+
+
+run(host='0.0.0.0', port=8080)
---
+++ api_post.py
@@ -0,0 +1,87 @@
+from mongoAtlasConnection import coll, db
+from bottle import route, run, get, post, request
+import datetime
+from bottle import response
+
+
+@post("/user/create")
+def creatUser():
+    '''crea un username y la inserta en la api'''
+    n = max(coll.distinct("idUser"))+1
+    names = (coll.distinct("userName"))
+    name = request.forms.get("userName")
+    if name in names:
+        return "Name already exists"
+    else:
+        info = {"userName": name,
+                "idUser": n}
+        coll.insert_one(info)
+        return f"User_id: {n}"
+
+
+@post("/chat/create")
+def creatChat():
+    '''crea un chat con usuarios y lo inserta en la api'''
+    c = max(coll.distinct("idChat"))+1
+    users_list = list(request.forms.getall("user_id"))
+    info = []
+    for u in users_list:
+        user = {}
+        user_info = (coll.find({"idUser": int(u)}, {"userName": 1}))
+        user["idUser"] = int(u)
+        user['userName'] = user_info[0]["userName"]
+        user['idChat'] = c
+        info.append(user)
+    coll.insert_many(info)
+    return f"Chat_id: {c}"
+
+
+@post("/chat/<chat_id>/adduser")
+def addUsertoChat(chat_id):
+    '''añade un usuario al chat indicado y lo inserta en la api'''
+    res = "ok"
+    user = request.forms.get("user_id")
+    user_info = list(coll.find({"idUser": int(user)}, {
+                     "userName": 1, "idChat": 1, "_id": 0}))
+    print(user_info)
+    for u in user_info:
+        if u["idChat"] == int(chat_id):
+            res = f"User {user} already in chat {chat_id}"
+    if res == "ok":
+        info = {
+            "idUser": int(user),
+            "userName": user_info[0]["userName"],
+            "idChat": int(chat_id)
+        }
+        coll.insert_one(info)
+        return f"Chat_id: {chat_id}"
+    else:
+        return res
+
+
+@post("/chat/<chat_id>/addmessage")
+def addMessagetoChat(chat_id):
+    '''añade un mensaje de un usuario en el chat indicado y lo inserta en la api'''
+    chat_id = int(chat_id)
+    user = int(request.forms.get("user_id"))
+    text = request.forms.get("text")
+    user_info = coll.find_one({"idUser": user, "idChat": chat_id}, {
+                              "userName": 1, "idChat": 1, "_id": 0})
+    message_id = max(coll.distinct("idMessage"))+1
+
+    if user_info and user_info["idChat"] == chat_id:
+        info = {
+            "idUser": user,
+            "userName": user_info[0]["userName"],
+            "idChat": chat_id,
+            "idMessage": int(message_id),
+            "datetime": datetime.datetime.utcnow(),
+            "text": text
+        }
+        coll.insert_one(info)
+        return {"Message_id": message_id}
+    else:
+        response.status = 500
+        return {
+            "error": "User does not exist in this chat"
+        }
